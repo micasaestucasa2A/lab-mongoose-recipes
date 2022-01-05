1fa9cf965592ff41d5865d205187bc123c3a707c
Nicolas Casabianca <nicolas_casabianca@hotmail.fr>
2022-01-05 18:06:02 +0100

    Solved except not getting exactly how to close after everything despite async function

--- index.js
+++ index.js
@@ -1,23 +1,71 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // Import of the model Recipe from './models/Recipe.model.js'
-const Recipe = require('./models/Recipe.model');
+const Recipe = require("./models/Recipe.model");
 // Import of the data from './data.json'
-const data = require('./data');
+const data = require("./data");
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = "mongodb://localhost:27017/recipe-app";
 
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI)
-  .then(x => {
+  .then((x) => {
     console.log(`Connected to the database: "${x.connection.name}"`);
     // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany()
+    return Recipe.deleteMany();
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    Recipe.create([{ title: "Dubitchou", cuisine: "bulgarian" }]);
+    Recipe.insertMany(data)
+      .then((newRecipe) => {
+        console.log("Great Success");
+        console.log(newRecipe);
+        modifyRecipe();
+        deleteCarrotCake();
+        closeDataBase();
+      })
+      .catch((notNewRecipe) => {
+        console.log("Big Failure !");
+        console.log(notNewRecipe);
+      });
   })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
+  .catch((error) => {
+    console.error("Error connecting to the database", error);
   });
+
+ //Iterations 4-5 functions 
+
+async function modifyRecipe() {
+  try {
+    const found = await Recipe.findOneAndUpdate(
+      { duration: 220 },
+      { duration: 100 },
+      { new: true }
+    );
+    console.log(
+      "Chef Luigi is a world class drunk, 100 min are enough if you don't pass out with Chianti",
+      found
+    );
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+function deleteCarrotCake() {
+  Recipe.findOneAndRemove({ name: "Carrot Cake" })
+    .then((notAvalaible) => {
+      console.log("No more available", notAvalaible);
+    })
+    .catch((err) => console.error(err));
+}
+
+async function closeDataBase() {
+  try {
+    mongoose.disconnect();
+    console.log("Database succesfully closed")
+  }
+  catch(err){
+    console.error(err)
+  }
+}
